e59b379e59fafb0e48e1775987042a38187cafde
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-12-04 10:48:05 +0530

    create types and exports

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Alert, Props as AlertProps } from './alert/Alert';
+export { Icon, Props as IconProps } from './icon/Icon';
+export { IconButton, Props as IconButtonProps } from './iconButton/IconButton';
